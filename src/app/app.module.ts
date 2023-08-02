bc4a527f6d1fc47175e0d376fdb789c496d993f3
Your Name <you@example.com>
2023-08-02 16:59:43 +0530

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { CustomaerComponent } from './customaer/customaer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    CustomaerComponent
   ],
   imports: [
     BrowserModule,
